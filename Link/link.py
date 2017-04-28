872f66b63f5c2c03845f947bc1f99876414122bc
Benjamin Southall <applemansigloo@gmail.com>
2017-04-28 14:17:50 +0900

    Add timeout support to requests.get in order to prevent bot disconnections caused when links to audio / video streams are pasted which cause network latency and hence ping timeouts

--- Link/link.py
+++ Link/link.py
@@ -2,9 +2,11 @@ from errbot.backends.irc import IRCRoom
 from errbot import BotPlugin, botcmd,  re_botcmd
 from itertools import chain
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import eventlet
 import re
 import requests
-from urllib.parse import urlparse
+eventlet.monkey_patch()
 
 # Regex is from last comment of https://gist.github.com/gruber/249502 , in order to match gitlai.in domain name etc 
 link_regex = re.compile('((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/?)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\)){0,}(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s\!()\[\]{};:\'\"\.\,<>?«»“”‘’]){0,})')
@@ -26,7 +28,7 @@ class Link(BotPlugin):
             if len(parsed_url.scheme) == 0:
                 url = "https://" + url 
             self.log.error("URL: " + url)
-            noembed = requests.get('https://noembed.com/embed', params={'url': url}, verify=False)
+            noembed = requests.get('https://noembed.com/embed', params={'url': url}, verify=False,timeout=30)
             resp = noembed.json()
             if 'error' not in resp.keys():
                 return_message += resp['title'].strip() + "\r\n"
@@ -36,12 +38,13 @@ class Link(BotPlugin):
                 if 'content-length' in head.headers and  int(head.headers['content-length']) > max_size:
                     return_message = "File too big for link preview\r\n"
                 else:
-                    response = requests.get(url)
-                    if response.status_code == 200:
-                        if 'text/html' in response.headers['content-type']:
-                            soup = BeautifulSoup(response.content,"lxml")
-                            return_message += soup.title.string + "\r\n"
-                        else:
-                            return_message += response.headers['content-type'] + " Size: " + response.headers['content-length'] + "\r\n"
+                    with eventlet.Timeout(60, False):
+                        response = requests.get(url,timeout=30)
+                        if response.status_code == 200:
+                            if 'text/html' in response.headers['content-type']:
+                                soup = BeautifulSoup(response.content,"lxml")
+                                return_message += soup.title.string + "\r\n"
+                            else:
+                                return_message += response.headers['content-type'] + " Size: " + response.headers['content-length'] + "\r\n"
         if len(return_message) > 0:
             self.send(msg.to, return_message)	
