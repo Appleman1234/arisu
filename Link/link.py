bbc7a403be5afda4bb8521c745db2a55b57e7a73
Benjamin Southall <applemansigloo@gmail.com>
2017-05-11 19:11:24 +0900

    Added noembed bypass suport for Twitter URLS in order to return  the correct titles.

--- Link/link.py
+++ Link/link.py
@@ -14,6 +14,32 @@ link_regex = re.compile('((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-
 class Link(BotPlugin):
     """Link plugin for Errbot"""
 
+    def get_noembed_title(self,url):
+        return_message = ""
+        noembed = requests.get('https://noembed.com/embed', params={'url': url}, verify=False,timeout=30)
+        resp = noembed.json()
+        if 'error' not in resp.keys():
+            return_message += resp['title'].strip() + "\r\n"
+        return return_message
+
+    def get_normal_title(self,url):
+        return_message = ""
+        head = requests.head(url)
+        max_size = 5e6
+        if 'content-length' in head.headers and  int(head.headers['content-length']) > max_size:
+            return_message = "File too big for link preview\r\n"
+        else:
+            with eventlet.Timeout(60, False):
+                response = requests.get(url,timeout=30)
+                if response.status_code == 200:
+                    if 'text/html' in response.headers['content-type']:
+                            soup = BeautifulSoup(response.content,"lxml")
+                            if soup.title is not None:
+                                return_message += soup.title.string + "\r\n"
+                    else:
+                        return_message += response.headers['content-type'] + " Size: " + response.headers['content-length'] + "\r\n"
+        return return_message
+
     def callback_message(self, msg):
         who = msg.frm.nick
         channel = None
@@ -27,24 +53,18 @@ class Link(BotPlugin):
             parsed_url = urlparse(url)
             if len(parsed_url.scheme) == 0:
                 url = "https://" + url 
+            no_embed_title = self.get_noembed_title(url)
+            normal_title = self.get_normal_title(url)
             self.log.error("URL: " + url)
-            noembed = requests.get('https://noembed.com/embed', params={'url': url}, verify=False,timeout=30)
-            resp = noembed.json()
-            if 'error' not in resp.keys():
-                return_message += resp['title'].strip() + "\r\n"
+            length_match = len(no_embed_title.strip('\r\n')) != len(url)
+            contains_twitter = "twitter" in url
+
+            if len(no_embed_title) == 0:
+               return_message += normal_title
             else:
-                head = requests.head(url)
-                max_size = 5e6
-                if 'content-length' in head.headers and  int(head.headers['content-length']) > max_size:
-                    return_message = "File too big for link preview\r\n"
-                else:
-                    with eventlet.Timeout(60, False):
-                        response = requests.get(url,timeout=30)
-                        if response.status_code == 200:
-                            if 'text/html' in response.headers['content-type']:
-                                soup = BeautifulSoup(response.content,"lxml")
-                                return_message += soup.title.string + "\r\n"
-                            else:
-                                return_message += response.headers['content-type'] + " Size: " + response.headers['content-length'] + "\r\n"
+               if contains_twitter:
+                   return_message += normal_title
+               else:
+                   return_message += no_embed_title
         if len(return_message) > 0:
             self.send(msg.to, return_message)	
